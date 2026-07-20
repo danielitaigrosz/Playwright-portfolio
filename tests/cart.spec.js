4a9ca2924f629c8fc561cf5c5b9aa8cce6e188cf
DaniGrosz <daniel.itaigrosz@gmail.com>
2026-07-20 17:19:46 -0300

    refatorando cart.spec.js para usar LoginFlow

--- tests/cart.spec.js
+++ tests/cart.spec.js
@@ -1,40 +1,43 @@
 // tests/cart.spec.js
 const { test, expect } = require('@playwright/test');
-const { LoginPage } = require('../pages/LoginPage');
-const { InventoryPage } = require('../pages/InventoryPage');
+//const { LoginPage } = require('../pages/LoginPage');
+//const { InventoryPage } = require('../pages/InventoryPage');
+const{LoginFlow} = require('../workflow/loginFlow');
 const users = require('../utils/users');
 
 test.describe('Carrinho de compras', () => {
-  let inventoryPage;
+  let loginflow;
 
   // Antes de cada teste, faz login com um usuário válido
   test.beforeEach(async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    inventoryPage = new InventoryPage(page);
+    //const loginPage = new LoginPage(page);
+    //inventoryPage = new InventoryPage(page);
+    loginflow = new LoginFlow(page);
 
-    await loginPage.goto();
-    await loginPage.login(users.standardUser.username, users.standardUser.password);
+    //await loginPage.goto();
+    //await loginPage.login(users.standardUser.username, users.standardUser.password);
+    await loginflow.logarComoUsuarioValido(users.standardUser.username, users.standardUser.password);
     await expect(page).toHaveURL(/inventory.html/);
   });
 
   test('deve adicionar um produto ao carrinho', async () => {
-    await inventoryPage.addProductToCartByName('Sauce Labs Backpack');
+    await loginflow.inventoryPage.addProductToCartByName('Sauce Labs Backpack');
 
-    const cartCount = await inventoryPage.getCartItemsCount();
+    const cartCount = await loginflow.inventoryPage.getCartItemsCount();
     expect(cartCount).toBe(1);
   });
 
   test('deve adicionar múltiplos produtos ao carrinho', async () => {
-    await inventoryPage.addProductToCartByName('Sauce Labs Backpack');
-    await inventoryPage.addProductToCartByName('Sauce Labs Bike Light');
-    await inventoryPage.addProductToCartByName('Sauce Labs Bolt T-Shirt');
+    await loginflow.inventoryPage.addProductToCartByName('Sauce Labs Backpack');
+    await loginflow.inventoryPage.addProductToCartByName('Sauce Labs Bike Light');
+    await loginflow.inventoryPage.addProductToCartByName('Sauce Labs Bolt T-Shirt');
 
-    const cartCount = await inventoryPage.getCartItemsCount();
+    const cartCount = await loginflow.inventoryPage.getCartItemsCount();
     expect(cartCount).toBe(3);
   });
 
   test('deve ordenar os produtos por preço (menor para maior)', async ({ page }) => {
-    await inventoryPage.sortBy('lohi');
+    await loginflow.inventoryPage.sortBy('lohi');
 
     const prices = await page.locator('.inventory_item_price').allTextContents();
     const numericPrices = prices.map((price) => parseFloat(price.replace('$', '')));
